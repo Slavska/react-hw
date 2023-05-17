193d6dd814f570863c7870c6cd237469f2cd388a
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-05-17 22:26:21 +0300

    Update all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,10 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './Transactions/TransactionHistory';
+// import { Profile } from './Profile/Profile';
+// import user from './Profile/user.json';
 
-import user from './Profile/user.json';
-import data from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './Transactions/transaction.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
     </div>
   );
 };
